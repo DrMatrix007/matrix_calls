45b4abf641b5a950922fc0de26aab4525f30eefa
Ofri Haviv <haviv.ofri@gmail.com>
2024-01-15 11:17:03 +0200

    added prisma

--- matrix_calls_client/src/hooks.server.ts
+++ matrix_calls_client/src/hooks.server.ts
@@ -2,29 +2,38 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
 import type { Handle } from "@sveltejs/kit";
 
-import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET, SPOTIFY_ID, SPOTIFY_SECRET, WEB_SECRET } from "$env/static/private"
+import {
+	GITHUB_ID,
+	GITHUB_SECRET,
+	GOOGLE_ID,
+	GOOGLE_SECRET,
+	SPOTIFY_ID,
+	SPOTIFY_SECRET,
+	WEB_SECRET,
+} from "$env/static/private";
 import GitHub from "@auth/core/providers/github";
 import Spotify from "@auth/core/providers/spotify";
 
-
 export const handle = SvelteKitAuth({
-  providers: [
-    Google({
-      clientId: GOOGLE_ID,
-      clientSecret: GOOGLE_SECRET,
-    }),
-    GitHub({
-      clientSecret: GITHUB_SECRET,
-      clientId: GITHUB_ID
-    }),
-    Spotify({
-      clientId: SPOTIFY_ID,
-      clientSecret: SPOTIFY_SECRET
-    }),
-  ],
-  callbacks: (req, a, b) => {
-
-  }
-  trustHost: true,
-  secret: WEB_SECRET
+	providers: [
+		Google({
+			clientId: GOOGLE_ID,
+			clientSecret: GOOGLE_SECRET,
+		}),
+		GitHub({
+			clientSecret: GITHUB_SECRET,
+			clientId: GITHUB_ID,
+		}),
+		Spotify({
+			clientId: SPOTIFY_ID,
+			clientSecret: SPOTIFY_SECRET,
+		}),
+	],
+	callbacks: {
+		jwt: (jwt, ..._data) => {
+			return jwt.token;
+		},
+	},
+	trustHost: true,
+	secret: WEB_SECRET,
 }) satisfies Handle;
