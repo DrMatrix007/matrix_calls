f1f1b6c53c50d09631fb85fe7fcad64159237f28
Ofri Haviv <haviv.ofri@gmail.com>
2024-04-30 08:33:34 +0300

    changed protocol

--- matrix_calls_client/src/lib/messages.ts
+++ matrix_calls_client/src/lib/messages.ts
@@ -7,7 +7,7 @@ class ServerSocket {
   constructor(jwt: string) {
     this.messageStore = writable("");
     // this.socket = new WebSocket("wss://matrix-calls-ws.vercel.app");
-    this.socket = new WebSocket("https://matrix-calls.onrender.com");
+    this.socket = new WebSocket("wss://matrix-calls.onrender.com");
     // this.socket = new WebSocket("ws://localhost:3000");
 
     this.socket?.addEventListener("open", (_event) => {
